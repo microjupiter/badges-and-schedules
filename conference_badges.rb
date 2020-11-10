640e9aece3ac7f28c684c168ecb7db553ccf669d
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-10 14:39:51 -0600

    commit tree 1a461f979648a60ef84f335cb3995766a5696908

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendees|
+    "Hello, my name is #{attendees}."
+  end
+end
+
+def assign_rooms(attendees)
+    attendees.each_with_index.map do |attendee, index|
+        "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendees).each do |assign|
+        puts assign
+    end
+end
